3ecda964b3b86430a56d026200b48cf362048caf
ano <anoidgit@users.noreply.github.com>
2018-09-20 11:08:28 +0800

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from chardet import detect
+
+def getEnc(bytes_in):
+	rs = detect(bytes_in)
+	return rs["encoding"], rs["confidence"]
+
+def decBytes(bytes_in, pdec = 0.8, ignore_err = True):
+	encm, p = getEnc(bytes_in)
+	if p >= pdec:
+		return bytes_in.decode(encm, ignore = ignore_err), True
+	else
+		return bytes_in, False
