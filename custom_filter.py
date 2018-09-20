e1686096a0cbb40a41bfbfd8f0ca0c0169661c3f
ano <hfxunlp@foxmail.com>
2018-09-20 14:31:52 +0800

    Accelerate webpage filter.

--- custom_filter.py
+++ custom_filter.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
-def is_WebPage(url, real_url):
+def is_WebPage(url, real_url = None):
 
-	false_fil = [".jpg", ".png", ".ico", ".gz", ".xz", ".bzip2", ".zip", ".rar", ".7z", ".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx", ".swf", ".mp3", ".mp4", ".mkv"]
+	true_filter = [".html", ".htm", ".php", ".asp"]
+	false_filter = [".jpg", ".png", ".ico", ".gz", ".xz", ".bzip2", ".zip", ".rar", ".7z", ".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx", ".swf", ".mp3", ".mp4", ".mkv"]
 
 	rs = True
+	if real_url is None:
+		real_url = url
 	real_url = real_url.lower()
 
-	for filter in false_fil:
+	for filter in true_filter:
+		if real_url.endswith(filter):
+			return True
+
+	for filter in false_filter:
 		if real_url.endswith(filter):
 			rs = False
 			break
